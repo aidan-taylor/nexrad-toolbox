31e6739d6f95f619b6e4d987d7965ed3ce47921c
Aidan Taylor <aidan.taylor@uea.ac.uk>
2025-04-21 15:07:13 +0100

    Optimised content of matfile
    Now stores the nexrad.core.Radar object generated from the binary file in the matfile rather then the raw data fields. Reduced load times and storage size.

--- toolbox/+nexrad/+conversions/pyLevel2Archive.m
+++ toolbox/+nexrad/+conversions/pyLevel2Archive.m
@@ -15,6 +15,7 @@ filename = nexrad.io.prepareForRead(filename, varargin{:});
 
 % Get tmp local path
 tempFolder = fullfile(tempdir, 'tmp_convert_nexrad');
+if ~isfolder(tempFolder), mkdir(tempFolder); end
 
 % Intialise output
 outputFiles = strings(1, length(filename));
@@ -23,17 +24,24 @@ for iFile = 1:length(filename)
 	% Get current dataset path (TODO -- could be from cloud? [20/03/2025])
 	sFile = filename(iFile);
 	
-	% Import NEXRAD Level II data
-	nexradData = py.pyart.io.read_nexrad_archive(sFile);
+	try
+		% Import NEXRAD Level II data
+		nexradData = py.pyart.io.read_nexrad_archive(sFile);
+		
+	catch
+		% If python errors, assume invalid archive, give warning and skip
+		warning('NEXRAD:IO:InvalidID', '"%s" is not a valid NEXRAD Level II archive, so skipping.', sFile);
+		continue
+	end
 	
 	% Get mapping to object fields
 	loopFields = fieldnames(nexradData);
 	% Get name without extension
 	[location, name] = fileparts(sFile);
 	
-	% Generate .mat file to write to
+	% Generate matfile to write to
 	finalFilename = append(fullfile(char(location), char(name)), '.mat');
-	radarData = matfile(finalFilename, "Writable", true);
+	radarFile = matfile(finalFilename, "Writable", true);
 	
 	for sDictionary = loopFields'
 		% Get the current dictionary name and contents
@@ -68,7 +76,7 @@ for iFile = 1:length(filename)
 			error('NEXRAD:PYTHON:CONVERSIONS:InvalidClass', '%s is currently an unsupported python class', class(currentDictionaryContents));
 		end
 		
-		% Access .mat file as object
+		% Access matfile as object
 		tempVar = load(currentDictionaryFilename);
 		
 		% If the dataset was not a dictionary, check for value field to extract
@@ -76,13 +84,77 @@ for iFile = 1:length(filename)
 			tempVar = tempVar.value;
 		end
 		
-		% Assign to final .mat file
+		% Assign to variable to construct object from
 		radarData.(currentDictionaryName) = tempVar;
 	end
+	
+	% Assign object to matfile
+	radarFile.radarObject = extractRadar(radarData);
 	
 	% Append current dataset filename to output
 	outputFiles(1, iFile) = finalFilename;
 end
 
 % Remove the tmp local folder
-rmdir(tempFolder, 's');
+if isfolder(tempFolder), rmdir(tempFolder, 's'); end
+
+
+%%
+function radar = extractRadar(radarData)
+%EXTRACTRADAR Extract variables and initialise nexrad.core.Radar class 
+%
+
+% Initialise optional inputs cell
+optionalInputs = cell.empty(1,0);
+
+% For each optional field, append the value to optionalInputs if it is given 
+if isfield(radarData, 'altitude_agl')
+	optionalInputs = [optionalInputs, 'altitude_agl', radarData.altitude_agl]; end
+if isfield(radarData, 'target_scan_rate')
+	optionalInputs = [optionalInputs, 'target_scan_rate', radarData.target_scan_rate]; end
+if isfield(radarData, 'rays_are_indexed')
+	optionalInputs = [optionalInputs, 'rays_are_indexed', radarData.rays_are_indexed]; end
+if isfield(radarData, 'ray_angle_res')
+	optionalInputs = [optionalInputs, 'ray_angle_res', radarData.ray_angle_res]; end
+if isfield(radarData, 'scan_rate')
+	optionalInputs = [optionalInputs, 'scan_rate', radarData.scan_rate]; end
+if isfield(radarData, 'antenna_transition')
+	optionalInputs = [optionalInputs, 'antenna_transition', radarData.antenna_transition]; end
+if isfield(radarData, 'instrument_parameters')
+	optionalInputs = [optionalInputs, 'instrument_parameters' radarData.instrument_parameters]; end
+if isfield(radarData, 'radar_calibration')
+	optionalInputs = [optionalInputs, 'radar_calibration', radarData.radar_calibration]; end
+if isfield(radarData, 'rotation')
+	optionalInputs = [optionalInputs, 'rotation', radarData.rotation]; end
+if isfield(radarData, 'tilt')
+	optionalInputs = [optionalInputs, 'tilt', radarData.tilt]; end
+if isfield(radarData, 'roll')
+	optionalInputs = [optionalInputs, 'roll', radarData.roll]; end
+if isfield(radarData, 'drift')
+	optionalInputs = [optionalInputs, 'drift', radarData.drift]; end
+if isfield(radarData, 'heading')
+	optionalInputs = [optionalInputs, 'heading', radarData.heading]; end
+if isfield(radarData, 'pitch')
+	optionalInputs = [optionalInputs, 'pitch', radarData.pitch]; end
+if isfield(radarData, 'georefs_applied')
+	optionalInputs = [optionalInputs, 'georefs_applied', radarData.georefs_applied]; end
+
+% Initialise radar object with all inputs
+radar = nexrad.core.Radar(...
+	radarData.time, ...
+	radarData.range, ...
+	radarData.fields, ...
+	radarData.metadata, ...
+	radarData.scan_type, ...
+	radarData.latitude, ...
+	radarData.longitude, ...
+	radarData.altitude, ...
+	radarData.sweep_number, ...
+	radarData.sweep_mode, ...
+	radarData.fixed_angle, ...
+	radarData.sweep_start_ray_index, ...
+	radarData.sweep_end_ray_index, ...
+	radarData.azimuth, ...
+	radarData.elevation, ...
+	optionalInputs{:} ...
+	);
