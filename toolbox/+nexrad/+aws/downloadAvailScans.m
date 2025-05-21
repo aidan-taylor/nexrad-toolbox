29424a3ec4c2e8f5624d9adfcf7775167a2156c6
Aidan Taylor <aidan.taylor@uea.ac.uk>
2025-05-21 17:33:19 +0100

    Updated AWS download function for parallelisation
    Parameterised thread count for parallel download. Add TODO reminder for later download error handling if necessary.

--- toolbox/+nexrad/+aws/downloadAvailScans.m
+++ toolbox/+nexrad/+aws/downloadAvailScans.m
@@ -1,11 +1,11 @@
-function [resultsPy, results] = downloadAvailScans(availscansPy, location, awsStructure)
+function [resultsPy, results] = downloadAvailScans(availScansPy, location, awsStructure, nThreads)
 %DOWNLOADAVAILSCANS Download selected scans to local drive and report success/fail metadata.
 %
 % Downloads the given scan data from the AWS NEXRAD Archive. Returns the metadata required to read the files from the
 % local system.
 %
 % INPUTS
-% availscans (py.list) - List containing the metadata for each of the scan available in the AWS NEXRAD archive. Fields
+% availScans (py.list) - List containing the metadata for each of the scan available in the AWS NEXRAD archive. Fields
 %									are: awspath, key, scan_time, radar_id, filename, last_modified (unused).
 % location (string) - Local drive path to desired download folder.
 % awsStructure (logical) - Whether or not to use the AWS folder structure inside the download location...
@@ -22,26 +22,21 @@ function [resultsPy, results] = downloadAvailScans(availscansPy, location, awsSt
 %
 
 arguments
-	availscansPy (1,:) py.list
+	availScansPy (1,:) py.list
 	location (1,1) string = pwd;
 	awsStructure (1,1) logical = true;
+	nThreads (1,1) double = 6;
 end
 
 % Initialise python AWS interface
 conn = py.nexradaws.NexradAwsInterface();
 
 % Attempt download of all available scan files
-resultsPy = conn.download(availscansPy, location, keep_aws_folders=awsStructure);
+resultsPy = conn.download(availScansPy, location, keep_aws_folders=awsStructure, threads=nThreads);
 
-% Check if any failed
-if double(resultsPy.failed_count) > 0
-	% Convert to see which have failed
-	failedList = nexrad.conversions.pyAwsNexradFile(resultsPy.failed);
-	
-	% TODO -- Why have they failed? Can they be tried again? [20/03/2025]
-	% TODO -- If get working, remove from failedList and add to success? .append to resultsPy.success before conversion?
-	% [20/03/2025]
-end
+% TODO Handle download failures?
+% if double(resultsPy.failed_count) > 0
+% end
 
 % Convert information to matlab friendly format if desired (usually called with [~, results])
 if nargout > 1
