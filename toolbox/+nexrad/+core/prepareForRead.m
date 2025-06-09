3e67605fe7e4b51844e9365ca4a7b7acbb0ea182
Aidan Taylor <aidan.taylor@uea.ac.uk>
2025-06-09 19:26:28 +0100

    Update prepareForRead.m
    Updated to reflect new python -> matlab Level II archive conversion no longer requiring matfile intermediates. Removed matfiles from valid archive list and so removed extended duplicate check function as recognising the same binary/matfile contents is no longer required. Also updated the warnings for invalid filenames.

--- toolbox/+nexrad/+core/prepareForRead.m
+++ toolbox/+nexrad/+core/prepareForRead.m
@@ -25,36 +25,37 @@ function filename = prepareForRead(filename, varargin)
 			
 		else
 			% Promp user to choose a folder(s) and/or file(s) (must be in same folder)
-			[filename] = nexrad.utility.uiget('Title', 'Choose NEXRAD Level II Data Archive(s)', 'MultiSelect', true);
+			filename = nexrad.utility.uiget('Title', 'Choose NEXRAD Level II Data Archive(s)', 'MultiSelect', true);
 			
-			% If location returns an "empty" string array ("" shows as 1x1 array) assume ui was cancelled and error
-			if isnumeric(filename), error('NEXRAD:IO:InvalidID', 'No local folder or file selected'); end
+			% If uiget returns 0, assume ui was cancelled and error
+			if isnumeric(filename), error("NEXRAD:IO:InvalidCode", "No local folder or file selected"); end
 		end
 	end
 	
-	% Now we have guaranteed file/folder names, check if a folder has been given and extract files
+	% Now we might have a mix of file/folder names, check if a folder(s) has
+	% been given and extract files 
 	if any(isfolder(filename))
-		filename = checkFolder(filename);
+		filename = extractFolder(filename);
 	end
 	
-	% Now we have only file names, check they are all real
+	% Now we have only file names, check they are all valid
 	filename = checkFile(filename);
 	
-	% Make compulsory final check to ensure there are no duplicates
-	filename = checkDuplicate(filename);
+	% Make compulsory final check to ensure there are no duplicates (absolute
+	% path only) 
+	filename = unique(filename);
 	
-	% If no valid files are left then give error
+	% If no files are left then error
 	if isempty(filename)
-		error('NEXRAD:IO:InvalidFileID', 'None of the original chosen files are valid');
+		error("NEXRAD:IO:InvalidFile", "None of the original chosen files are valid");
 	end
 	
 	
 	%%
-function filename = checkFolder(filename)
-	% CHECKFOLDER
-	%
+function filename = extractFolder(filename)
+	%EXTRACTFOLDER Extract likely NEXRAD Level II archive files from a folder(s)
 	
-	% Get idx of folders
+	% Get index of folders
 	validFolder = isfolder(filename);
 	
 	% Make new list and remove entries from filename
@@ -63,9 +64,8 @@ function filename = checkFolder(filename)
 	
 	% Loop over the folders given
 	for sFolder = foldername
-		% Get the complete list of files in the folder (currently assumes all files
-		% in the folder are correct binary Level 2 data files)
-		% TODO add ability to specify a subset?
+		% Get the complete list of files in the folder (this is a recursive
+		% check of all sub-folders)
 		fileList = dir(fullfile(sFolder, '**', '*'));
 		isFile = ~[fileList.isdir];
 		
@@ -77,10 +77,11 @@ function filename = checkFolder(filename)
 		newFilename = fullfile(pathList, nameList);
 		filename = [filename, newFilename]; %#ok<AGROW>
 		
-		% Perform initial check that the file is probably a correct level 2 file (files with no
-		% extension may be valid along with .gz compression and matfiles)
+		% Perform initial check that files are probably a correct level 2 file
+		% (files with no extension may be valid along with .gz compression).
+		% This is silent as the folders might contain many spurious files.
 		[~, ~, extension] = fileparts(filename);
-		validArchive = any([cellfun(@isempty, extension); endsWith(extension, [".gz", ".mat"])], 1);
+		validArchive = any([cellfun(@isempty, extension); endsWith(extension, ".gz")], 1);
 		
 		% Pass only valid archives to output
 		filename = filename(validArchive);
@@ -89,14 +90,18 @@ function filename = checkFolder(filename)
 	
 	%%
 function filename = checkFile(filename)
-	% CHECKFILE
-	%
+	% CHECKFILE Checks that given file(s) are valid and are likely NEXRAD Level
+	% II archives.
 	
 	% Get valid files and give warning for invalid files
 	validFile = isfile(filename);
 	
 	if any(~validFile)
-		warning('NEXRAD:IO:InvalidID', '"%s" is either not a valid file or not on the path so skipping...\n\t', filename{~validFile});
+		warning("backtrace", "off")
+		for sFile = filename(~validFile)
+			warning("NEXRAD:IO:InvalidFile", "'%s' is either not a valid file or not on the path so skipping...",sFile);
+		end
+		warning("backtrace", "on");
 	end
 	
 	% Pass only valid files to output
@@ -105,46 +110,15 @@ function filename = checkFile(filename)
 	% Perform check that the file is probably a correct level 2 file (files with no
 	% extension may be valid along with .gz compression and matfiles)
 	[~, ~, extension] = fileparts(filename);
-	validArchive = any([cellfun(@isempty, extension); endsWith(extension, [".gz", ".mat"])], 1);
+	validArchive = any([cellfun(@isempty, extension); endsWith(extension, ".gz")], 1);
 	
 	if any(~validArchive)
-		warning('NEXRAD:IO:InvalidID', '"%s" is not a valid archive, so skipping...\n\t', filename{~validArchive});
+		warning("backtrace", "off")
+		for sFile = filename(~validArchive)
+			warning("NEXRAD:IO:InvalidFile", "'%s' is not a valid archive, so skipping...", sFile);
+		end
+		warning("backtrace", "on");
 	end
 	
 	% Pass only valid archives to output
 	filename = filename(validArchive);
-	
-	
-	%%
-function filename = checkDuplicate(filename)
-	% CHECKDUPLICATE
-	%
-	
-	% Check the files ending in .mat to prevent duplication if the binary is also present
-	isMat = endsWith(filename, '.mat');
-	nonMatFilename = filename(~isMat);
-	matFilename = filename(isMat);
-	
-	% Ensures that this only happens if the binary is also present
-	% (prioritise the matfiles for conversion efficiency)
-	duplicateIdx = zeros(length(matFilename), length(nonMatFilename));
-	
-	for iFile = 1:length(nonMatFilename)
-		% Take fileparts of binary files to remove potential extensions
-		[location, name] = fileparts(nonMatFilename(iFile));
-		
-		% Check if any of the matfile names contain their binary equivalent
-		duplicateIdx(:, iFile) = startsWith(matFilename, fullfile(location, name));
-	end
-	
-	% Form final array which stores the raw binary to pass through if the
-	% corresponding .mat file has not also been given
-	nonDuplicate = nonMatFilename(~any(duplicateIdx, 1));
-	
-	% Overwrite final list (appends the matfiles given, and
-	% any binary files that are not duplicates of the corresponding matfiles)
-	filename = [matFilename, nonDuplicate];
-	
-	% Make unique check (removes any duplicate files with same extension, also sorts
-	% in alphabetical)
-	filename = unique(filename);
