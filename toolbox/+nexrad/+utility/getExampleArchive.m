8bff3bc72782139c92def4c0262ad7a85088e8cf
Aidan Taylor <aidan.taylor@uea.ac.uk>
2025-06-12 12:01:17 +0100

    Hotfix to getExampleArchive
    Now creates the examples folder to save  the example into first if it doesn't already exist.

--- toolbox/+nexrad/+utility/getExampleArchive.m
+++ toolbox/+nexrad/+utility/getExampleArchive.m
@@ -2,8 +2,10 @@ function filename = getExampleArchive
 	%GETEXAMPLEARCHIVE Downloads the example NEXRAD Level II archive file used
 	% in the gettingStarted guide and returns the absolute path.
 	
-	filename = fullfile(nexrad.utility.getRootFolder, "examples/KMHX20180914_111837_V05");
+	exampleFolder = fullfile(nexrad.utility.getRootFolder, "examples");
+	filename = fullfile(exampleFolder, "KMHX20180914_111837_V06");
 	
 	if ~isfile(filename)
+		if ~isfolder(exampleFolder), mkdir(exampleFolder); end
 		websave(filename, "https://github.com/aidan-taylor/nexrad-toolbox/raw/refs/heads/main/examples/KMHX20180914_111837_V06");
 	end
